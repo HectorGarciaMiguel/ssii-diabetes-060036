0222735dd2ee34717a9c027211742f37b6f4ec76
hectorgarciamiguel@gmail.com <hectorgarciamiguel@gmail.com@6e9590d7-1440-d5b9-d7b2-6c48088276d2>
2013-04-26 21:27:38 +0000

    inicial

---
+++ SeguimientoDiabetes/src/logica_negocio/Alimentacion.java
@@ -0,0 +1,19 @@
+package logica_negocio;
+
+public class Alimentacion {
+
+	private String ingestaAdicional;
+	private String dieta;
+	private int calorias;
+	
+	private String conclusion;
+
+	
+	public Alimentacion(){
+		
+	}
+	
+	public String getConclusion(){
+		return conclusion;
+	}
+}
---
+++ SeguimientoDiabetes/src/logica_negocio/DosisInsulina.java
@@ -0,0 +1,21 @@
+package logica_negocio;
+
+public class DosisInsulina {
+
+	private int cantidad;
+	private int tipoInsulina;
+	private String zonaPinchazo;
+	
+	private String conclusion;
+	
+	
+	public DosisInsulina(){
+				
+	}
+	
+	public String getConclusion(){
+		return conclusion;
+	}
+	
+	
+}
